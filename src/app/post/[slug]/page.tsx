bdd2faeb3a0647d9354329ec9524af99796bc868
vnfma0218 <vnfma0218@naver.com>
2023-05-16 23:55:19 +0900

    feat: app directory 생성 및 페이지 이동, 포스트 상세페이지 진행중

---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import { AiOutlineEye } from 'react-icons/ai';
+
+export default function page() {
+  return (
+    <div className="max-w-5xl mx-auto">
+      <h1>상세페이지</h1>
+      {/* user info, page info */}
+      <div className="">
+        <div className="flex">
+          <div className="rounded-full bg-gray-400 border w-12 h-12 mr-2"></div>
+          <div>
+            <p>닉네임</p>
+            <div className="flex items-center text-gray-700 text-sm">
+              <span className="mr-2">약 2시간 전</span>
+              <div className="flex items-center">
+                <AiOutlineEye />
+                <span className="ml-1">121</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* 내용 */}
+      <div className="h-96 border border-black">
+        <h1>내용 들어가야함</h1>
+      </div>
+      <div className="mt-2 divider"></div>
+
+      {/* 댓글 */}
+      <div>
+        <p>179개의 댓글</p>
+        <div className="flex border p-5 w-full">
+          <div className="w-2/12">
+            <div className="rounded-full bg-gray-400 border w-12 h-12 mr-2"></div>
+          </div>
+          <div className="w-full relative">
+            {/* 입력 input (text editor) */}
+            <div className="border w-full h-20"></div>
+            <button className="btn btn-sm absolute right-0 mt-4 px-6 ">
+              댓글쓰기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
